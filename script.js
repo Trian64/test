5f4386e4e8de78ba93cb9709bc1d4687546bfc4c
Nathan <148826741+Trian64@users.noreply.github.com>
2025-01-28 18:25:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,2 @@
 window.onload = function() {
     fetch('QRScout_config (1).json')
-        .then(response => response.json())
-        .then(data => {
-            let output = "<ul>";
-            data.forEach(item => {
-                output += `<li>${item.name} - ${item.value}</li>`; // Adjust this based on the structure of your JSON
-            });
-            output += "</ul>";
-            document.getElementById('data-container').innerHTML = output;
-        })
-        .catch(error => console.error('Error fetching JSON:', error));
