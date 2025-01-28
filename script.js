3fdd6d642241b17ca6f0c8d96f0f4d825a327bcc
Nathan <148826741+Trian64@users.noreply.github.com>
2025-01-28 18:14:26 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    fetch('data.json')
+        .then(response => response.json())
+        .then(data => {
+            let output = "<ul>";
+            data.forEach(item => {
+                output += `<li>${item.name} - ${item.value}</li>`; // Adjust this based on the structure of your JSON
+            });
+            output += "</ul>";
+            document.getElementById('data-container').innerHTML = output;
+        })
+        .catch(error => console.error('Error fetching JSON:', error));
